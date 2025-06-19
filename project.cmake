8b79fac08425b271968fbd9293c5de654bf6e938
kdizzlle <kelly.mary.williams@gmail.com>
2025-06-18 17:26:33 -0700

    add scales-fprime-lib submodule

--- project.cmake
+++ project.cmake
@@ -2,5 +2,6 @@
 # This allows for reuse between deployments, or other projects.
 
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Components")
-add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/ImxDeployment/")
-add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/JetsonDeployment/")
+# add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/ImxDeployment/")
+# add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/JetsonDeployment/")
+# add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/lib/scales-fprime-lib/scales/scalesSvc/")
