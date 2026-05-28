a289d662f98ca85f1fe65c57c0780ec46721dbe7
Dat Nguyen <bidat2007@gmail.com>
2026-05-27 19:04:14 -0700

    Fixed Jetson Lab Deployment

--- JetsonDeployment/Top/JetsonDeploymentTopology.cpp
+++ JetsonDeployment/Top/JetsonDeploymentTopology.cpp
@@ -29,7 +29,7 @@ Svc::FprimeDeframing deframing;
 Svc::FprimeFraming hubFraming;
 Svc::FprimeDeframing hubDeframing;
 
-const char* REMOTE_HUIP_ADDRESS = "10.3.2.2"; // ip of JPL IMX
+const char* REMOTE_HUIP_ADDRESS = "10.3.2.11"; // ip of JPL IMX
 // const char* REMOTE_HUIP_ADDRESS = "10.3.2.6"; // ip of CPP IMX
 const U32 REMOTE_HUPORT = 50500;
 
